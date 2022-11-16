8ec6f1c76536883c698b231922ee57489129d326
zakariaCreate <70917892+zakariaCreate@users.noreply.github.com>
2022-11-16 10:10:20 -0500

    Update buidswitches.sh

--- useful_files/buidswitches.sh
+++ useful_files/buidswitches.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
 
 cd /home/vagrant/behavioral-model
-echo -e '\n\n* we are here : *\n\n'
-pwd
-echo -e '\n\n'
-sleep 0.5
 ./autogen.sh
-./configure
-
-echo -e '\n\n* we are here : *\n\n'
-pwd
-echo -e '\n\n'
-
-sleep 0.5
-
-make && sudo make install && sudo ldconfig \
-&& cd targets/simple_switch_grpc \
-&& echo -e '\n\n* we are here : *\n\n' \
-&& pwd \
-&& echo -e '\n\n' \
-&& sleep 0.5 \
-&& make && sudo make install && sudo ldconfig \
-&& cd ../.. \
-&& echo -e '\n\n* we are here : *\n\n' \
-&& pwd \
-&& echo -e '\n\n' ;
+./configure --enable-debugger --with-pi
+make
+# Only run the following two commands if there were no errors from the previous command:
+sudo make install
+sudo ldconfig
+# Simple_switch_grpc target
+cd targets/simple_switch_grpc
+# ./autogen.sh
+./configure --with-thrift
+make
+# Only run the following two commands if there were no errors from the previous command:
+sudo make install
+sudo ldconfig
